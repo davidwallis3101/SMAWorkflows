90a050d63d0e7b0213cc10029d6bb5194b6bd053
david_wallis_cp <SND\david_wallis_cp>
2015-07-06 07:51:07 +0000

    Fixed mix of tabs and spaces.

--- Misc/Send-SMS.ps1
+++ Misc/Send-SMS.ps1
@@ -5,43 +5,43 @@ David Wallis 04/07/2015 smaworkflows.codeplex.com
 #>
 workflow Send-SMS
 {
-	param (
-		# Phone Number
-    	[parameter(Mandatory=$true)]
+    param (
+        # Phone Number
+        [parameter(Mandatory=$true)]
         [string]$number,
 
-		# Message contents
+        # Message contents
         [parameter(Mandatory=$true)]
         [string]$message,
 
-		# Originator
-		[parameter(Mandatory=$false)]
-		[string]$originator
+        # Originator
+        [parameter(Mandatory=$false)]
+        [string]$originator
     )
 
-	$url = "https://gw1.aql.com/sms/postmsg.php"
+    $url = "https://gw1.aql.com/sms/postmsg.php"
 
     #Get credentials for connecting to the aql api  
     $Credentials = Get-AutomationPSCredential -Name 'AQLCredentials'
-	
-	# Get the username and password
-	$Password = $Credentials.GetNetworkCredential().Password
-	$Username = $Credentials.Username
-		
-	# Construct the URL and submit the request
-	$uri = "$($url)?to_num=$($number)&message=$($message)&flash=0&orig=$($originator)&username=$($Username)&password=$($Password)"
-	$response = Invoke-WebRequest -Uri $uri -Method Post -ContentType "application/x-www-form-urlencoded" -UseBasicParsing #-Proxy http://myproxyserver:8080 
+    
+    # Get the username and password
+    $Password = $Credentials.GetNetworkCredential().Password
+    $Username = $Credentials.Username
+        
+    # Construct the URL and submit the request
+    $uri = "$($url)?to_num=$($number)&message=$($message)&flash=0&orig=$($originator)&username=$($Username)&password=$($Password)"
+    $response = Invoke-WebRequest -Uri $uri -Method Post -ContentType "application/x-www-form-urlencoded" -UseBasicParsing #-Proxy http://myproxyserver:8080 
 
-	# Check the response content
-	switch -CaseSensitive ($response.Content) {
-		"AQSMS-OK" {
-			Write-Output "Message sent successfully to $($number)"
-		}
-		"AQSMS-AUTHERROR" {
-			Write-Error "Authentication Error."
-		}
-		default {
-			Write-Output "Unknown Response: $($response.Content)"
-		}
-	}
+    # Check the response content
+    switch -CaseSensitive ($response.Content) {
+        "AQSMS-OK" {
+            Write-Output "Message sent successfully to $($number)"
+        }
+        "AQSMS-AUTHERROR" {
+            Write-Error "Authentication Error."
+        }
+        default {
+            Write-Output "Unknown Response: $($response.Content)"
+        }
+    }
 }
